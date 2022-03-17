5d8e94496f747445581ae5e0a0a9a2706f05e999
Guille <guillermolopez3@gmail.com>
2022-03-16 23:25:15 -0300

    add packages

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+export 'package:logger/logger.dart';
+export 'package:http/http.dart';
+export 'package:url_launcher/url_launcher.dart';
+export 'package:camera/camera.dart';
+export 'package:shared_preferences/shared_preferences.dart';
+export 'package:google_maps_flutter/google_maps_flutter.dart';
+export 'package:image_picker/image_picker.dart';
+export 'package:intl/intl.dart';
+export 'package:package_info/package_info.dart';
+export 'package:connectivity/connectivity.dart';
+export 'package:device_info/device_info.dart';
+export 'package:path_provider/path_provider.dart';
+export 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+export 'package:super_tooltip/super_tooltip.dart';
+
+//firebase
+export 'package:firebase_core/firebase_core.dart';
+export 'package:firebase_crashlytics/firebase_crashlytics.dart';
+export 'package:firebase_messaging/firebase_messaging.dart';
+export 'package:firebase_analytics/firebase_analytics.dart';
+export 'package:dio_firebase_performance/dio_firebase_performance.dart';
+
+//another team
+export 'package:flutter_bloc/flutter_bloc.dart';
+export 'package:google_fonts/google_fonts.dart';
+export 'package:carousel_slider/carousel_slider.dart';
+export 'package:dotted_border/dotted_border.dart';
+export 'package:geolocator/geolocator.dart';
+export 'package:geocoding/geocoding.dart';
+export 'package:cached_network_image/cached_network_image.dart';
+export 'package:badges/badges.dart';
+export 'package:equatable/equatable.dart';
+export 'package:clevertap_plugin/clevertap_plugin.dart';
+export 'package:flutter_html/flutter_html.dart';
+export 'package:flutter_svg/flutter_svg.dart';
+export 'package:shimmer/shimmer.dart';
+export 'package:screenshot/screenshot.dart';
+export 'package:image_gallery_saver/image_gallery_saver.dart';
+export 'package:flutter_branch_sdk/flutter_branch_sdk.dart';
+export 'package:uni_links/uni_links.dart';
+export 'package:flutter_google_places/flutter_google_places.dart';
+
+export 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+export 'package:painter/painter.dart';
+export 'package:time_range_picker/time_range_picker.dart';
+export 'package:flutter_modular/flutter_modular.dart';
+export 'package:native_screenshot/native_screenshot.dart';
+export 'package:dart_ipify/dart_ipify.dart';
+export 'package:modular_interfaces/modular_interfaces.dart';
+export 'package:collection/collection.dart';
+export 'package:native_updater/native_updater.dart';
+export 'package:in_app_review/in_app_review.dart';
+export 'package:multiple_localization/multiple_localization.dart';
+export 'package:algolia/algolia.dart';
+export 'package:barcode_widget/barcode_widget.dart';
+
+//dev dependencies
+export 'package:bloc_test/bloc_test.dart';
+export 'package:mocktail/mocktail.dart';
+export 'package:build_runner/build_runner.dart';
+export 'package:modular_test/modular_test.dart';
